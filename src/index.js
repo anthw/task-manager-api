45aefe04cb3ab8671ef62e378a53a75ef1931876
Anth Winter <anthwinter@gmail.com>
2019-07-14 21:38:39 +0100

    Process env updates

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import userRouter from './routers/user'
 import taskRouter from './routers/task'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // Auto parse incoming JSON
 app.use(express.json())
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
   try {
     // We only want the token, strip the rest.
     const token = req.header('Authorization').replace('Bearer ', '')
-    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
     const user = await User.findOne({ _id: decodedToken._id, 'tokens.token': token })
 
     if (!user) {
